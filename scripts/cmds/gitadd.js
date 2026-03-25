a1492bb50ac373708c9362aa077e043825b019fc
ayanokojisai35-blip <ayanokojisai35@gmail.com>
2026-03-25 19:35:33 +0600

    Update gitadd.js

--- scripts/cmds/gitadd.js
+++ scripts/cmds/gitadd.js
@@ -34,7 +34,7 @@ module.exports = {
 
  const githubToken = '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';
  const owner = 'ayanokojisai35-blip';
- const repo = 'Nobody-';
+ const repo = 'Johan';
  const branch = 'main';
  const filePath = path.join('scripts', 'cmds', fileName);
 
